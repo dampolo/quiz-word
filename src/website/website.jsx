8bc4699be99712245da221243be3a27c93304bd2
Damian Poloczek <damianpoloczek@gmail.com>
2026-05-17 22:19:46 +0200

    hero secion ready

--- src/website/website.jsx
+++ src/website/website.jsx
@@ -1,5 +1,66 @@
+import "./website.scss";
+
+
 function Website() {
-    return <h1>Website</h1>
+  return (
+    <section className="hero">
+      <div className="hero__container">
+        {/* Left Content */}
+        <div className="hero__content">
+          <div className="hero__badge">✦ Progress Focused Learning</div>
+
+          <h1 className="hero__title">
+            Welcome on <span>quiz word</span>
+          </h1>
+
+          <p className="hero__description">
+            on this webpage you can learn languages as you want and where you
+            want. Master any language with cognitive clarity and scientifically
+            backed methods.
+          </p>
+
+          <div className="hero__buttons">
+            <a href="#" className="hero__button hero__button--primary">
+              Register
+              <span>→</span>
+            </a>
+
+            <a href="#" className="hero__button hero__button--secondary">
+              Login
+            </a>
+          </div>
+        </div>
+
+        {/* Right Image */}
+        <div className="hero__image-wrapper">
+          <div className="hero__image-card">
+            <img
+              src="./assets/learning-board.png"
+              alt="Learning Board"
+              className="hero__image"
+            />
+
+            {/* Floating Card */}
+            <div className="hero__floating-card">
+              <div className="hero__floating-top">
+                <div className="hero__avatars">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+
+                <p>15k+ Learners online</p>
+              </div>
+
+              <div className="hero__progress">
+                <div className="hero__progress-bar"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Website;
