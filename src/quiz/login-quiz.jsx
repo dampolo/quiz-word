6a4d478a164d15a8d6df5ee05877caee6b34bf06
Damian Poloczek <damianpoloczek@gmail.com>
2026-06-23 22:45:01 +0200

    login styles works

--- src/quiz/login-quiz.jsx
+++ src/quiz/login-quiz.jsx
@@ -4,66 +4,66 @@ import { Link } from "react-router-dom";
 import "./login-quiz.scss";
 
 function LoginQuiz() {
-    const initialValues = { email: "", password: "" };
+  const initialValues = { email: "", password: "" };
 
-    const [formValues, setFormValues] = useState(initialValues);
-    
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value});
-        console.log(formValues);
-        
-    }
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-    const loginWithEmailAndPassword = (e) => {
-        e.preventDefault()
-    }
+  const [formValues, setFormValues] = useState(initialValues);
 
-    return (
-        <section className="main-content-customer">
-        <div className="form-title">
-            <h1 className="form-title-name">Anmeldung</h1>
-        </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    console.log(formValues);
+  };
 
-        <p className="description">
-            Wir empfehlen dir, die E-Mail-Adresse zu nutzen, die du bei der Arbeit
-            verwendest.
-        </p>
-        <p className="description">
-            Hast du kein konto?
-            <Link className="new-user-link" to="/kurse/create-account">
-            Konto erstellen
-            </Link>
-        </p>
+  const loginWithEmailAndPassword = (e) => {
+    e.preventDefault();
+  };
 
-        <form onSubmit={loginWithEmailAndPassword}>
-            <div className="input-container">
+  return (
+    <section className="main-content-customer">
+      <div className="form-title">
+        <h1 className="form-title-name">Anmeldung</h1>
+      </div>
 
-                <pre>{JSON.stringify(formValues , undefined)}</pre>
-            
-            <label htmlFor="email">E-Mail-Adresse</label>
-            <input
-                className="input-field"
-                type="email"
-                name="email"
-                placeholder="beispielname@email.com"
-                autoComplete="email"
-                value={formValues.email}
-                onChange={handleChange}
+      <p className="description">
+        Wir empfehlen dir, die E-Mail-Adresse zu nutzen, die du bei der Arbeit
+        verwendest.
+      </p>
+      <p className="description">
+        Hast du kein konto?
+        <Link className="new-user-link" to="/kurse/create-account">
+          Konto erstellen
+        </Link>
+      </p>
+
+      <form onSubmit={loginWithEmailAndPassword}>
+        <div className="input-container">
+          <pre>{JSON.stringify(formValues, undefined)}</pre>
+
+          <label htmlFor="email">E-Mail-Adresse</label>
+          <input
+            className="input-field"
+            type="email"
+            name="email"
+            placeholder="beispielname@email.com"
+            autoComplete="email"
+            value={formValues.email}
+            onChange={handleChange}
+          />
+
+          <div className="input-icon">
+            <img
+              width="24"
+              height="24"
+              aria-hidden="true"
+              src="./assets/mail-icon-input-field.svg"
+              alt=""
             />
+          </div>
 
-            <div className="input-icon">
-                <img
-                width="24"
-                height="24"
-                aria-hidden="true"
-                src="./assets/mail-icon-input-field.svg"
-                alt=""
-                />
-            </div>
-
-            <div className="warn-txt">
-                {/* <span>{errorResponse()}</span>
+          <div className="warn-txt">
+            {/* <span>{errorResponse()}</span>
 
                 {userEmailInvalid && (
                 <>
@@ -75,49 +75,49 @@ function LoginQuiz() {
                     )}
                 </>
                 )} */}
-            </div>
-            </div>
+          </div>
+        </div>
 
-            <div className="input-container">
-            <label htmlFor="password">Passwort</label>
-            <input
-                autoComplete="current-password"
-                className="input-field"
-                // type={isPasswordVisible ? "text" : "password"}
-                name="password"
-                placeholder="Passwort"
-                value={formValues.password}
-                onChange={handleChange}
+        <div className="input-container">
+          <label htmlFor="password">Passwort</label>
+          <input
+            autoComplete="current-password"
+            className="input-field"
+            type={isPasswordVisible ? "text" : "password"}
+            name="password"
+            placeholder="Passwort"
+            value={formValues.password}
+            onChange={handleChange}
+          />
+
+          <button
+            type="button"
+            className="eye-button"
+            onClick={() => setIsPasswordVisible((prev) => !prev)}
+          >
+            <img
+              width="24"
+              height="24"
+              className="pwd-eye"
+              src={
+                isPasswordVisible ? "./assets/eye.svg" : "./assets/eye-off.svg"
+              }
+              alt={isPasswordVisible ? "verstecken" : "zeigen"}
             />
+          </button>
 
-            <button
-                type="button"
-                className="eye-button"
-                // onClick={togglePasswordVisibility}
-            >
-                <img
-                width="24"
-                height="24"
-                className="pwd-eye"
-                // src={
-                //     isPasswordVisible ? "./assets/eye.svg" : "./assets/eye-off.svg"
-                // }
-                // alt={isPasswordVisible ? "verstecken" : "zeigen"}
-                />
-            </button>
+          <div className="input-icon">
+            <img
+              width="24"
+              height="24"
+              aria-hidden="true"
+              src="./assets/pwd-lock-icon-input-field.svg"
+              alt=""
+            />
+          </div>
 
-            <div className="input-icon">
-                <img
-                width="24"
-                height="24"
-                aria-hidden="true"
-                src="./assets/pwd-lock-icon-input-field.svg"
-                alt=""
-                />
-            </div>
-
-            <div className="warn-txt warn-txt-hight">
-                {/* {passwordInvalid && (
+          <div className="warn-txt warn-txt-hight">
+            {/* {passwordInvalid && (
                 <>
                     {passwordRequired && (
                     <small>Password ist ein Pflichtfeld.</small>
@@ -135,35 +135,31 @@ function LoginQuiz() {
                     )}
                 </>
                 )} */}
-            </div>
-            </div>
+          </div>
+        </div>
 
-            {/* <Preloader
+        {/* <Preloader
             className={
                 mainStateService.showPreloader ? "show-preloader" : "hide-preloader"
             }
             /> */}
 
-            <div className="btn-container">
-            <button
-                className="forward-btn main-btn-start-in-krypto"
-                type="submit"
-            >
-                Anmelden
-            </button>
-            </div>
+        <div className="btn-container">
+          <button className="main-quiz-button" type="submit">
+            Anmelden
+          </button>
+        </div>
 
-            <Link className="btn-password" to="/kurse/forgot-password">
-            Passwort vergessen?
-            </Link>
-
+        <Link className="btn-password" to="/kurse/forgot-password">
+          Passwort vergessen?
+        </Link>
+        {/* 
             <div className="seperator-container">
             <span className="sep-line-txt">ODER</span>
-            </div>
-        </form>
-
-        </section>
-    );
+            </div> */}
+      </form>
+    </section>
+  );
 }
 
 export default LoginQuiz;
