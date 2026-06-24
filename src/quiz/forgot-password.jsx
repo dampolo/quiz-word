f5e212d0adb0d8e9cded256f970e268adb7e4aee
Damian Poloczek <damianpoloczek@gmail.com>
2026-06-24 22:35:25 +0200

    forgot password in progress

--- src/quiz/forgot-password.jsx
+++ src/quiz/forgot-password.jsx
@@ -1,5 +1,104 @@
+import { useState } from "react";
+import "./forgot-password.scss";
+import { Link } from "react-router-dom";
+
 function ForgotPassword() {
-    return <h1>Passwort zurücksetzen</h1>
+  const initialValues = { email: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+
+  const recoveryForm = (e) => {
+    e.preventDefault();
+
+    const errors = validate(formValues);
+    setFormErrors(errors);
+    setFormValues(initialValues)
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    const updatedValues = {
+      ...formValues,
+      [name]: value,
+    };
+
+    setFormValues(updatedValues);
+    setFormErrors(validate(updatedValues));
+  };
+
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.email || !regexEmail.test(values.email)) {
+      errors.email = "E-Mail ist unvollständig/inkorrekt.";
+    }
+
+    return errors;
+  };
+  
+  const isEmailValid = regexEmail.test(formValues.email);
+
+  return (
+    <section className="main-content-customer">
+      <Link className="arrow-back" to="/login" aria-label="Anmelden">
+        <app-back></app-back>
+      </Link>
+
+      <div className="form-title">
+        <h1 className="form-title-name">Passwort vergessen</h1>
+      </div>
+
+      <form className="form" onSubmit={recoveryForm}>
+        <div className="input-container">
+          <label htmlFor="email">Bitte gib deine E-Mail ein:</label>
+
+          <input
+            className="input-field"
+            type="email"
+            name="email"
+            placeholder="beispielname@email.com"
+            autoComplete="email"
+            value={formValues.email}
+            onChange={handleChange}
+          />
+
+          <div className="input-icon">
+            <img
+              width="24"
+              height="24"
+              aria-hidden="true"
+              src="./assets/mail-icon-input-field.svg"
+              alt=""
+            />
+          </div>
+
+          <div className="warn-txt">{formErrors.email}</div>
+        </div>
+
+        <p className="description">
+          Wir senden Dir eine E-Mail, <br />
+          über die Du dein Passwort ändern kannst.
+        </p>
+
+        <div className="btn-container">
+          <a className="back-button" routerLink="/customer/profile">
+            Abbrechen
+          </a>
+
+          <button
+            className="main-quiz-button"
+            type="submit"
+            disabled={!isEmailValid}
+          >
+            E-Mail senden
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default ForgotPassword;
