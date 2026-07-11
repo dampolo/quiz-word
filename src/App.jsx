163d3499fbd7bfd7433f5bb01d67be9d0dff23fd
Damian Poloczek <damianpoloczek@gmail.com>
2026-07-11 07:51:27 +0200

    change the router

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ import AddNewQuiz from "./quiz/my-quiz/quizzes/add-new-quiz";
 import Quizzes from "./quiz/my-quiz/quizzes/quizzes";
 import AllQuizWords from "./quiz/my-quiz/quizzes/all-quiz-words";
 
-
 function App() {
   return (
     // Auth
@@ -46,31 +45,32 @@ function App() {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="profile" element={<Profile />} />
 
-          <Route element={<VocabularyProvider>
-            <DialogProvider>
-            <Outlet />
-            </DialogProvider>
-            
-            </VocabularyProvider>}>
+          <Route
+            element={
+              <VocabularyProvider>
+                <QuizProvider>
+                  <DialogProvider>
+                    <Outlet />
+                  </DialogProvider>
+                </QuizProvider>
+              </VocabularyProvider>
+            }
+          >
+            {/* Vocabulary */}
             <Route path="all-words" element={<AllWords />} />
             <Route path=":id/edit-word" element={<EditWord />} />
             <Route path="add-new-word" element={<AddNewWord />} />
-            <Route path="vocabulary-categories" element={<VocabularyCategories />} />
+            <Route path="vocabulary-categories"element={<VocabularyCategories />}
+            />
             <Route path="add-new-category" element={<AddNewCategory />} />
             <Route path=":id/edit-category" element={<EditCategory />} />
-          </Route>
-          
-          <Route element={<QuizProvider>
-            <DialogProvider>
-            <Outlet />
-            </DialogProvider>
-            
-            </QuizProvider>}>
+
+            {/* Quizzes */}
             <Route path="quizzes" element={<Quizzes />} />
             <Route path="add-new-quiz" element={<AddNewQuiz />} />
             <Route path=":id/all-quiz-words" element={<AllQuizWords />} />
           </Route>
-
+          
         </Route>
       </Route>
     </Routes>
