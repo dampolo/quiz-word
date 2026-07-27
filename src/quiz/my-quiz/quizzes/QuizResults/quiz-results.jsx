4484f0f62457424139dcce35bcf2e1b57f47b2b7
Damian Poloczek <damianpoloczek@gmail.com>
2026-07-27 13:52:51 +0200

    optimization quiz-results

--- src/quiz/my-quiz/quizzes/QuizResults/quiz-results.jsx
+++ src/quiz/my-quiz/quizzes/QuizResults/quiz-results.jsx
@@ -1,50 +1,65 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams, Link } from "react-router-dom";
+import "./quiz-results.scss"
+import BackButton from "../../../../components/BackButton/BackButton";
 
 function QuizResults() {
+  const { id } = useParams();
   const { state } = useLocation();
   console.log(state);
-  
+
   const details = state?.results || [];
 
   return (
-    <div className="vocabulary-card">
-      <div className="card-header">
-        <h3>Deine Ergebnise</h3>
-        <span className="badge">{details.length} Wörter</span>
+    <section className="results">
+        <BackButton to={`/my-quiz/${id}/all-quiz-words/`} />
+      <div className="vocabulary-card">
+
+        <div className="card-header">
+          <h3>Deine Ergebnise</h3>
+          <span className="badge">{details.length} Wörter</span>
+        </div>
+
+        <div className="table">
+          {details.map((item) => (
+            <div
+              key={item.id}
+              className={`table-row ${!item.is_correct ? "wrong" : ""}`}
+            >
+              <div className="status">
+                <span
+                  className={item.is_correct ? "icon success" : "icon error"}
+                >
+                  {item.is_correct ? "✓" : "✕"}
+                </span>
+              </div>
+
+              <div className="column">
+                <span className="label">SOURCE WORD</span>
+                <h4>{item.correct_answer}</h4>
+              </div>
+
+              <div className="column">
+                <span className="label">YOUR ANSWER</span>
+                <p className={!item.is_correct ? "incorrect" : ""}>
+                  {item.user_answer}
+                </p>
+              </div>
+
+              <div className="column">
+                <span className="label">CORRECT MEANING</span>
+                <p className="correct">{item.correct_answer}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-
-      <div className="table">
-        {details.map((item) => (
-          <div
-            key={item.id}
-            className={`table-row ${!item.is_correct ? "wrong" : ""}`}
-          >
-            <div className="status">
-              <span className={item.is_correct ? "icon success" : "icon error"}>
-                {item.is_correct ? "✓" : "✕"}
-              </span>
-            </div>
-
-            <div className="column">
-              <span className="label">SOURCE WORD</span>
-              <h4>{item.correct_answer}</h4>
-            </div>
-
-            <div className="column">
-              <span className="label">YOUR ANSWER</span>
-              <p className={!item.is_correct ? "incorrect" : ""}>
-                {item.user_answer}
-              </p>
-            </div>
-
-            <div className="column">
-              <span className="label">CORRECT MEANING</span>
-              <p className="correct">{item.correct_answer}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+        <Link
+          className="main-quiz-button add-btn"
+          to={`/my-quiz/${id}/play-quiz`}
+        >
+          Wiederholen
+        </Link>
+    </section>
   );
 }
 
