7d69acc7b28d4d83cfbcbe279932edad8463d8b4
Damian Poloczek <damianpoloczek@gmail.com>
2026-07-28 22:22:26 +0200

    price styles

--- src/website/price.jsx
+++ src/website/price.jsx
@@ -1,6 +1,8 @@
+import "./price.scss"
+
 function Price(params) {
     return (
-        <main>
+        <main className="main-price">
             <section className="price">
                 <h1 className="title">Preise</h1>
                 <p>Quiz word is komplett kostenlos.</p>
