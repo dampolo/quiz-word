dc255d56a56557e83d09461ac6f1ac839626c0bf
Damian Poloczek <damianpoloczek@gmail.com>
2026-07-29 08:18:49 +0200

    help desk update

--- src/website/help-desk.jsx
+++ src/website/help-desk.jsx
@@ -11,13 +11,10 @@ function HelpDesk() {
         <div className="help-desk-list-content">
           <details>
             <summary class="help-desk-button collapsible">
-              Ist jetzt der richtige Zeitpunkt, um einzusteigen?
+              Was kostet der Quiz Word?
             </summary>
             <div class="help-desk-list-content">
-              Einen perfekten Zeitpunkt gibt es nicht. Wichtiger als Timing ist
-              ein sinnvoller Einstieg mit Struktur. Wer langfristig investieren
-              möchte und schrittweise beginnt, für den ist es sinnvoll, jetzt
-              mit dem ersten kleinen Schritt zu starten.
+                Der Quiz word is komplett kostenlos.
             </div>
           </details>
         </div>
