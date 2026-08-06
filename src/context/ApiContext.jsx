ddeb5819afc948899ca26346b491de9b50782948
Damian Poloczek <damianpoloczek@gmail.com>
2026-08-06 13:13:36 +0200

    Changed APIContext

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext } from 'react';
 
 
-const BASE_URL = import.meta.env.API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 // React nutzt diesen Wert immer dann, wenn KEIN Provider existiert
 const ApiContext = createContext(BASE_URL);
